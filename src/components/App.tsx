4c1d7b3dd9fae235da7ad909e11411f7bb7c89c3
utsho <utshodebravestone@gmail.com>
2023-11-25 23:23:50 +0600

    added navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import About from "./About";
 import Intro from "./Intro";
+import Navigation from "./Navigation";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
 
 const App = () => {
   return (
-    <div className="py-5 md:py-10 min-h-screen min-w-full bg-black text-white">
-      <div className="max-w-[750px] mx-2.5 md:mx-auto">
-        <Intro />
-        <div className="h-5 md:h-10" />
-        <About />
+    <BrowserRouter>
+      <div className="pt-5 md:pt-10 min-h-screen min-w-full bg-black text-white">
+        <div className="max-w-[750px] mx-2.5 md:mx-auto">
+          <Intro />
+          <div className="h-5 md:h-10" />
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<About />} />
+          </Routes>
+          <div className="h-5" />
+          <Navigation />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav className="sticky left-0 bottom-0 w-full py-5 border-t border-neutral-700 rounded-t-2xl z-50 bg-neutral-950">
+      <ul className="flex justify-center gap-5">
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              (isActive ? "text-yellow-500 font-semibold" : "text-white") +
+              " hover:text-yellow-500 transition-all duration-300"
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/portfolio"
+            className={({ isActive }) =>
+              (isActive ? "text-yellow-500 font-semibold" : "text-white") +
+              " hover:text-yellow-500 transition-all duration-300"
+            }
+          >
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              (isActive ? "text-yellow-500 font-semibold" : "text-white") +
+              " hover:text-yellow-500 transition-all duration-300"
+            }
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
