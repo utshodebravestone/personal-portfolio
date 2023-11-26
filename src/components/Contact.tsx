4a72b697f971adfa410017800b86501b56e57ccc
utsho <utshodebravestone@gmail.com>
2023-11-26 11:23:32 +0600

    added form functionality

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,26 @@
 import { BsFillSendFill } from "react-icons/bs";
+import useForm from "../hooks/useForm";
 
 const Contact = () => {
+  const onSubmit = ({
+    name,
+    email,
+    message,
+  }: {
+    name: string;
+    email: string;
+    message: string;
+  }) => {
+    window.open(
+      `mailto:utshodebravestone@gmail.com?cc=${email}&subject=Message from ${name.toUpperCase()} (via Utsho's Personal Website)&body=${message}`
+    );
+  };
+
+  const { formData, handleInputChange, handleSubmit } = useForm(
+    { name: "", email: "", message: "" },
+    onSubmit
+  );
+
   return (
     <section className="bg-neutral-900 w-full py-5 px-2.5 md:px-10 flex flex-col gap-2.5 md:gap-5 rounded-xl border border-neutral-700">
       <h1 className="font-bold text-2xl md:text-4xl">Contact</h1>
@@ -12,11 +32,11 @@ const Contact = () => {
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d459755.8402334259!2d89.33904070615144!3d25.80876958544507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e2c8648556f8cb%3A0xfaa7d471019abb1f!2sKurigram%20District!5e0!3m2!1sen!2sbd!4v1700969307794!5m2!1sen!2sbd"
         loading="lazy"
-        className="w-full h-80 md:h-96 grayscale-100 invert rounded"
+        className="w-full h-80 md:h-96 grayscale-100 invert rounded-md"
       ></iframe>
 
       <form
-        onSubmit={(e) => e.preventDefault()}
+        onSubmit={handleSubmit}
         className="mt-4 flex flex-col items-center justify-center gap-5"
       >
         <h1 className="w-full text-left text-xl md:text-[1.75rem] font-semibold">
@@ -28,14 +48,21 @@ const Contact = () => {
             required
             minLength={4}
             placeholder="Full Name"
-            className="p-2 w-full md:w-1/2 h-12 font-normal bg-transparent border rounded-md text-gray-100 outline-none [&:not(:placeholder-shown)]:invalid:border-red-500 valid:border-green-500"
+            className="p-2 w-full md:w-1/2 h-12 font-normal bg-neutral-900 border border-neutral-700 rounded-md text-gray-100 outline-none [&:not(:placeholder-shown)]:invalid:border-red-500 valid:border-green-500"
+            autoComplete="false"
+            onChange={handleInputChange}
+            value={formData.name}
+            name="name"
           />
           <input
             type="email"
             required
             minLength={8}
             placeholder="Email Address"
-            className="p-2 w-full md:w-1/2 h-12 font-normal bg-transparent border rounded-md text-gray-100 outline-none [&:not(:placeholder-shown)]:invalid:border-red-500 valid:border-green-500"
+            className="p-2 w-full md:w-1/2 h-12 font-normal bg-neutral-900 border border-neutral-700 rounded-md text-gray-100 outline-none [&:not(:placeholder-shown)]:invalid:border-red-500 valid:border-green-500"
+            onChange={handleInputChange}
+            value={formData.email}
+            name="email"
           />
         </div>
         <textarea
@@ -43,7 +70,10 @@ const Contact = () => {
           required
           minLength={16}
           placeholder="Message"
-          className="w-full p-2 font-normal bg-transparent border rounded-md text-gray-100 outline-none [&:not(:placeholder-shown)]:invalid:border-red-500 valid:border-green-500"
+          className="w-full p-2 font-normal bg-neutral-900 border border-neutral-700 rounded-md text-gray-100 outline-none [&:not(:placeholder-shown)]:invalid:border-red-500 valid:border-green-500"
+          onChange={handleInputChange}
+          value={formData.message}
+          name="message"
         ></textarea>
         <div className="w-full flex justify-end">
           <button className="w-full md:w-fit flex justify-center items-center gap-2 px-4 py-2 border border-neutral-800 shadow-md shadow-neutral-800 rounded-lg text-yellow-400 font-bold hover:translate-y-0.5 hover:contrast-150 transition-all duration-300">
---
+++ src/hooks/useForm.ts
@@ -0,0 +1,25 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+const useForm = <T extends object>(
+  initialState: T,
+  onSubmit: (_: T) => void
+): {
+  formData: T;
+  handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
+} => {
+  const [formData, setFormData] = useState(initialState);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  return { formData, handleInputChange, handleSubmit };
+};
+
+export default useForm;
